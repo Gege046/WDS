64e19fd90cc386cfa9ddbdc2507c1878a2612287
Gege046 <103126377+Gege046@users.noreply.github.com>
2022-04-06 18:15:42 +0400

    Add files via upload

---
+++ Digital Future Studio/Js/app.js
@@ -0,0 +1,56 @@
+const header = document.querySelector('.header')
+const button = document.querySelector('.button')
+const logo = document.querySelector('.header__logo span')
+
+var index = false
+
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0) {
+        header.classList.add('active')
+        button.classList.add('active')
+    }else {
+        if(index == false) {
+            header.classList.remove('active')
+        }
+        button.classList.remove('active')
+    }
+    console.log(window.scrollY)
+})
+
+ //Burger 
+
+  const burgerBtn = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+
+  burgerBtn.addEventListener('click', () => {
+      if(index == false) {
+          header.classList.add('active')
+          menu.classList.add('active')
+          index = true
+      }else{
+          if(window.screenY == 62) {
+            header.classList.remove('active')
+        }
+        console.log(window.screenY)
+            menu.classList.remove('active')
+            index = false
+      }
+      
+  })
+
+var swiper = new Swiper(".slider", {
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+ 
