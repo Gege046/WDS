00ea0de06c8425c0c0d09150e68e66501cf849ef
Gege046 <103126377+Gege046@users.noreply.github.com>
2022-04-09 14:15:08 +0400

    Add files via upload

--- Js/app.js
+++ Js/app.js
@@ -7,51 +7,85 @@ var index = false
 
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 0) {
+    if (window.scrollY > 0) {
         header.classList.add('active')
         button.classList.add('active')
-    }else {
-        if(index == false) {
+    } else {
+        if (index == false) {
             header.classList.remove('active')
         }
         button.classList.remove('active')
     }
-    console.log(window.scrollY)
+})
+window.addEventListener('load', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('active')
+        button.classList.add('active')
+    }
 })
 
- //Burger 
+//Burger 
 
-  const burgerBtn = document.querySelector('.burger')
-  const menu = document.querySelector('.menu')
+const burgerOpen = document.querySelector('.burger')
+const burgerClose = document.querySelector('.close')
+const menu = document.querySelector('.burger__menu')
+const navigation = document.querySelectorAll('.list__item-item')
 
-  burgerBtn.addEventListener('click', () => {
-      if(index == false) {
-          header.classList.add('active')
-          menu.classList.add('active')
-          index = true
-      }else{
-          if(window.screenY == 0) {
-            header.classList.remove('active')
-        }
-        console.log(window.screenY)
-            menu.classList.remove('active')
-            index = false
-      }
-      
-  })
+burgerOpen.addEventListener('click', () => {
+    menu.classList.add('active')
+})
+
+burgerClose.addEventListener('click', () => {
+    menu.classList.remove('active')
+})
+
+navigation.forEach(links => {
+    links.addEventListener('click', () => {
+        menu.classList.remove('active')
+    })
+})
+
+
 
 var swiper = new Swiper(".slider", {
     centeredSlides: true,
-    loop:true,
+    loop: true,
     autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
+        delay: 4000,
+        disableOnInteraction: false,
     },
+    pagination: {
+        el: ".swiper-pagination",
+      },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-  });
+});
 
 
- 
+//Portfolio Showmore
+
+const showmoreButton = document.getElementById('portfolio__button-btn')
+var current = 9;
+
+showmoreButton.onclick = () => {
+    event.preventDefault()
+    let item = [...document.querySelectorAll('.portfolio__items .portfolio__item')]
+
+    try {
+        for (var i = current; i < current + 6; i++) {
+            item[i].classList.add('show')
+        }
+    } catch (error) {}
+
+    current += 6
+    console.log(current)
+
+
+    if (current >= item.length) {
+        showmoreButton.style.display = 'none'
+    }
+
+
+}
